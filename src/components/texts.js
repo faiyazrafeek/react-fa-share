ae9eff9e7daf042b325b8adc644e1467f1c7aad0
Faiyaz Rafeek <fayaznahwi@gmail.com>
2021-04-07 00:03:17 +0530

    temp deploy

---
+++ src/components/texts.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './texts.css'
+import { Clipboard } from 'react-feather';
+
+
+function Texts(message) {
+    const messages = Object.keys(message.message).map(id =>  message.message[id] )
+
+    if (messages.length === 0) {
+        return <h4>No Data yet...</h4>
+    }
+
+    return (        
+        <div className="card-container">
+            {messages.map((msg, id) => 
+                {
+                    return(             
+                        <div key={id} className="card mb-3">                               
+                            <div> <button onClick={copyToClipboard}><Clipboard/></button> </div>
+                            <div className="card-body"> { msg.message } </div>
+                        </div>
+                    )
+                }
+            )}
+        </div>
+    )
+}
+
+export default Texts
+
