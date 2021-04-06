9415c041a9b15a4041431aa3d1265c836df2a075
Faiyaz Rafeek <fayaznahwi@gmail.com>
2021-04-07 00:22:39 +0530

    copy bug fixed

--- src/components/texts.js
+++ src/components/texts.js
@@ -5,6 +5,7 @@ import { Clipboard } from 'react-feather';
 
 function Texts(message) {
     const messages = Object.keys(message.message).map(id =>  message.message[id] )
+    
 
     if (messages.length === 0) {
         return <h4>No Data yet...</h4>
@@ -15,9 +16,9 @@ function Texts(message) {
             {messages.map((msg, id) => 
                 {
                     return(             
-                        <div key={id} className="card mb-3">                               
-                            <div> <button><Clipboard/></button> </div>
-                            <div className="card-body"> { msg.message } </div>
+                        <div key={id} className="card mb-2">                               
+                       
+                            <div className="card-body">{  } { msg.message } </div>
                         </div>
                     )
                 }
